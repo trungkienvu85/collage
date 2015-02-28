4337c8e8c00cfd12d6942024614a4723dd8ecbeb
absimas <ab.simas@gmail.com>
2015-02-28 18:23:57 +0200

    utils class's main methods

--- app/src/main/java/com/example/simas/collage/MainActivity.java
+++ app/src/main/java/com/example/simas/collage/MainActivity.java
@@ -23,6 +23,7 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity
 		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
+
 	/**
 	 * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
 	 */
